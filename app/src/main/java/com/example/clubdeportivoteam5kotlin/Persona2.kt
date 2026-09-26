91e610df5590fcb34dd3142d8f822bccd38aa6e7
JuanHotasegui <jmhotasegui@gmail.com>
2026-09-26 00:15:52 -0300

    Se completo el hilo de registro de persona en xml. Fix: agrego gradle-wrapper.properties faltante y elimino gradle-daemon-jvm.properties conflictivo

---
+++ app/src/main/java/com/example/clubdeportivoteam5kotlin/Persona2.kt
@@ -0,0 +1,20 @@
+package com.example.clubdeportivoteam5kotlin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Persona2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_persona2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.persona2)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
